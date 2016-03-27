f644f27bbc3409dce6b8a845954ae55aee42c0fe
JamesLinus <www.1464217939@outlook.com>
2016-03-27 08:09:47 +0800

    Create one-click cleanup.sh
    markdown

---
+++ osx/one-click cleanup.sh
@@ -0,0 +1,12 @@
+/应用程序/
+~/资源库/Application Support/
+~/资源库/Preferences/
+~/资源库/Caches/
+~/资源库/Containers
+~/资源库/LaunchAgents/
+~/资源库/PreferencePanes/
+~/下载
+/System/Library/LaunchDaemons/
+/System/Library/LaunchAgents
+
+mdfind -name "mac软件宝箱"

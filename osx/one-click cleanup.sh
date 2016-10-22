caf1bde3c0f354bc9778295d2fcc256f31e4f595
JamesLinus <www.1464217939@outlook.com>
2016-10-22 14:52:39 +0800

    add complete clean list

--- osx/one-click cleanup.sh
+++ osx/one-click cleanup.sh
@@ -1,12 +1,27 @@
-/应用程序/
-~/资源库/Application Support/
-~/资源库/Preferences/
-~/资源库/Caches/
-~/资源库/Containers
-~/资源库/LaunchAgents/
-~/资源库/PreferencePanes/
-~/下载
-/System/Library/LaunchDaemons/
-/System/Library/LaunchAgents
+echo /Application/
+sudo rm -rf ~/Library/Application Support/*
+sudo rm -rf ~/Library/Preferences/*
+sudo rm -rf ~/Library/Caches/*
+sudo rm -rf ~/Library/Containers/*
+sudo rm -rf ~/Library/LaunchAgents/*
+sudo rm -rf ~/Library/PreferencePanes/*
+sudo rm -rf ~/Downloads/*
+sudo rm -rf /System/Library/LaunchDaemons/*
+sudo rm -rf /System/Library/LaunchAgents/*
+sudo rm -rf /Library/Application\ Support/App\ Store/*
+sudo rm -rf /Library/Application\ Support/CrashReporter/*
+sudo rm -rf /Library/Caches/*
+sudo rm -rf /Library/Receipts/*
+sudo rm -rf /Library/Updates/*
+sudo rm -rf /System/Library/Caches/*
+sudo rm -rf /System/Library/CacheDeletes/*
+sudo rm -rf /Users/Shared/*
+sudo rm -rf /Users/apple/Documents/*
+sudo rm -rf /Users/apple/Downloads/*
+sudo rm -rf /Users/apple/Desktop/*
+sudo rm -rf /Users/apple/Pictures/*
+sudo rm -rf /Users/apple/Movies/*
+sudo rm -rf /Users/apple/Music/*
 
-mdfind -name "mac软件宝箱"
+sudo rm -rf /private/var/logs/*
+sudo purge

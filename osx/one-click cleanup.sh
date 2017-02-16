c3b73040bd8ceb9a2d7284c5a0a3275600f3771c
JamesLinus <www.1464217939@outlook.com>
2017-02-16 03:59:31 -0800

    add reset spotlight command line.

--- osx/one-click cleanup.sh
+++ osx/one-click cleanup.sh
@@ -1,4 +1,6 @@
 echo /Application/
+
+
 sudo rm -rf ~/Library/Application Support/*
 sudo rm -rf ~/Library/Preferences/*
 sudo rm -rf ~/Library/Caches/*
@@ -22,6 +24,14 @@ sudo rm -rf /Users/apple/Desktop/*
 sudo rm -rf /Users/apple/Pictures/*
 sudo rm -rf /Users/apple/Movies/*
 sudo rm -rf /Users/apple/Music/*
+
+sudo mdutil -E /                         ### 重建Spotlight索引
+sudo mdutil -E /Volumes/Macintosh\ HD/
+sudo mdutil -E /Volumes/External/
+mdimport /path/to/file
+
+// http://www.macx.cn/thread-2035117-1-1.html
+
 
 sudo rm -rf /private/var/logs/*
 sudo purge
